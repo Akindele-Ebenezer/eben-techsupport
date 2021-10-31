d7f181a65afc2b3b9d571aa4cc6af820c52a0969
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-10-31 20:41:18 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php    
-$conn = mysqli_connect('us-cdbr-east-04.cleardb.com', 'b59d129ca43733', '3d3e894e', 'epiz_30164177_akindeleebenezer_portal'); 
+$conn = mysqli_connect('us-cdbr-east-04.cleardb.com', 'b59d129ca43733', '3d3e894e', 'akindeleebenezer_portal'); 
 //$sql = mysqli_query($conn, $query);
 
 ?>
