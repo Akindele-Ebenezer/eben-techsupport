68b5bb22bfbd7be90bc8644381bd72b6a066e524
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-10-31 21:10:49 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php    
-$conn = mysqli_connect('us-cdbr-east-04.cleardb.com', 'b59d129ca43733', '3d3e894e', 'akindeleebenezer_portal'); 
+$conn = mysqli_connect('us-cdbr-east-04.cleardb.com', 'b59d129ca43733', '3d3e894e', 'heroku_8800e42de2c026c'); 
 //$sql = mysqli_query($conn, $query);
 
 ?>
