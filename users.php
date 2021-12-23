bae7c3d6775a21922823e6b56524c2aadf14008e
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-12-23 10:52:38 +0100

    Update users.php

--- users.php
+++ users.php
@@ -100,7 +100,7 @@ $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 		<div>
 			<h1><?= $users['first_name'] . ' ' . $users['last_name']; ?> <br> <img src="<?= $users['profile_pic']; ?>"></h1>
 			<br>
-			<a href="chat.php">Message</a>
+			<!-- <a href="chat.php">Message</a> -->
 			<ul>
 				<li><h3>SCHOOL</h3> <br><br> <span><?= $users["school"]; ?></span></li>
 				<li><h3>CLASS</h3> <br><br> <span><?= $users["class"]; ?></span></li> 
