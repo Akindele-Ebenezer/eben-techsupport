9c49ea00ddde0b91702e39a2c7519879ea1eadaa
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-12-27 09:04:29 +0100

    Add files via upload

---
+++ change-name.php
@@ -0,0 +1,56 @@
+ <?php 
+error_reporting(0);
+	include "config.php";
+	include "login.php";
+	$title = "Change Name";
+	$page_title = $title;
+	include "page.php"; 
+	
+	error_reporting(0);
+	
+	$id = $_POST["id"];
+	$username = $_SESSION["username"];
+	$change_first_name = $_POST["change_first_name"];
+	$change_last_name = $_POST["change_last_name"];
+	$email = $_POST["email"];
+	$tel_no = $_POST["tel_no"];
+	
+	$sql = "UPDATE users SET first_name = '$change_first_name', last_name = '$change_last_name' WHERE id = $id;";
+	$query = mysqli_query($conn, $sql);
+	//$result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+	
+ ?>
+ <div class="content-wrapper">
+	<div class="dashboard">
+		<ul>
+			<li><a href="profile.php">My Dashboard</a></li>
+			<li><a href="contact.php">Contact</a></li>					
+			<li><a href="users.php">Students</a></li>
+			<li><a href="notification.php">Notification</a></li>
+			<li><a href="change-name.php">Change Name</a></li>	
+			<li><a href="change-profile-picture.php">Change Profile Picture</a></li>
+			<li><a href="change-password.php">Change Password</a></li>
+			<li><a href="email-correction.php">Email Correction</a></li>
+			<li><a href="logout.php">Log Out</a></li>
+		</ul>
+	</div> 
+	<form action="<?php $_SERVER["PHP_SELF"]; ?>" method="post">
+		<div id="change-name-wrapper">
+			<div id="change-name-content">
+				<h1>Change Name</h1>
+				<p>It's quick and easy</p> 
+				<hr>
+				<input type="number" name="id" placeholder="User Id"> 
+				<input type="text" name="change_first_name" placeholder="First name (New)">
+				<input type="text" name="change_last_name" placeholder="Last name (New)"> <br>
+				<input type="email" name="email" placeholder="Email"> 
+				<input type="number" name="tel_no" placeholder="Tel No.">  <br>
+				<button type="submit">Change Name</button> 
+			</div> 
+		</div>
+	</form>
+ </div>
+ <?php
+	include "footer.html";
+?>
+
---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?= $page_title; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link rel="stylesheet" href="styles.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">  
+ 	<link rel="icon" href="images/company-logo.png">
+</head>
+     
+<body>
+	<header id="lg-screen-header">
+		<div id="top-info-wrappper">
+			<div id="top-info"> 
+				<a href="index.php">
+					<div id="logo">
+						<span>E</span><span>TS</span>
+					</div>
+				</a>
+					<div id="address">
+						<img src="images/address.PNG">
+						<p><strong>ADDRESS</strong> <br> LAGOS, NIGERIA</p>
+					</div>
+					<div id="call-us">
+						<a href="tel:09052757805"><img id="call" src="images/call.PNG"></a>
+						<p><a href="tel:09052757805"><strong>CALL US</strong></a> <br> 09052757809</p> 
+					</div>
+					<div id="social-links">
+						<a href='mailto:tobi.akindele@gmail.com' target='_blank'><img src="images/mail.png"></a>
+						<a href='https://www.facebook.com/akindele.ebenezer/' target='_blank'><img src="images/fb.png" id="fb"></a>
+						<a href='#'><img src="images/ws.png"></a>
+						<a href='https://www.instagram.com/ebenezer__/?hl=en' target='_blank'><img src="images/ig.jpg"></a>
+					</div>
+			</div>
+		</div>
+		
+		<nav>
+			<ul id="top-nav">
+				<li><a href="index.php">HOME</a></li>
+				<li><a href="profile.php">DASHBOARD</a></li>
+				<li><a href="notification.php">NOTIFICATION</a></li>
+				<li><a href="location.php">LOCATION</a></li>
+				<li><a href="contact.php">CONTACT</a></li> 
+				<li><a href="logout.php">LOG OUT</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<?php include "header-sm-screen.html"; ?>
+ 
