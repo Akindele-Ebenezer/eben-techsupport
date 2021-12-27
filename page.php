81b2ab04eac8b3175ae3602707ac9330310e6f28
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2021-12-27 09:13:22 +0100

    Add files via upload

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php include "header.php";?>
+ 
+	
+	<div id="welcome-wrapper">
+		<img src="images/bg.jpg">
+		<img src="images/bg.jpg">
+		
+		<div id="welcome-text"> 
+			<h1><?= $title; ?></h1>
+			<p>We are also available to help you build and implement pleasant/responsive websites. <br> <strong><a href="https://akindele-ebenezer.herokuapp.com" target="_blank">Click Here</a></strong> to know more..</p>			
+		</div> 
+	</div>
+	
+ 
+ 
