8849f1849ae8bffb8c1be27fecab71fc9ab7c691
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-01-13 08:12:02 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,9 +1,15 @@
+/*
 <?php
+  include "login.php";
   include "config.php";
-  $SQL = "SELECT * FROM 
+  $SQL = "SELECT * FROM users WHERE username = 'ebenkey';";
+  $query = mysqli_query($conn, $sql2);  
+  $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
   include "header.php";
 ?>
+   
 
 <?php
   include "footer.php";
 ?>
+*/
