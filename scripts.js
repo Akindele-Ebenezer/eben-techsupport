d9d28efb6e58cd66b32f2f336d106576a5ac3344
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-01-15 16:18:20 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 const navIcon = document.querySelector("#nav-icon img");
-const smNavLinks = document.querySelector(".sm-nav-links");
- //console.log(smNavLinks);
- 
-navIcon.addEventListener("click", () => {
-	smNavLinks.classList.toggle('show'); 
-});
- 
+const smNavLinks = document.querySelector(".sm-nav-links");  
 
+ //console.log(body)
+	 navIcon.addEventListener("click", () => {
+	 smNavLinks.classList.toggle('show'); 
+	}); 
+
+	footer.addEventListener("click", () => {
+		smNavLinks.classList.remove('show');
+	});
+ 
